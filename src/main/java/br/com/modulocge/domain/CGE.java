55667459e3c92289926daffbac3e7cb707816388
Douglas Lima <douglasproglima@gmail.com>
2016-05-14 16:26:49 -0300

    Fabrica de Sessão e GenericDomain()

---
+++ src/main/java/br/com/modulocge/domain/CGE.java
@@ -0,0 +1,186 @@
+package br.com.modulocge.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@SuppressWarnings("serial")
+@Entity
+public class CGE extends GenericDomain{
+	@Column(length = 100, nullable = false)
+	private String nome;
+
+	@Column(length = 100)
+	private String nomeFantasia;
+	
+	@Column(length = 40)
+	private String contato;
+
+	@Column(length = 18)
+	private String cnpj;	
+	
+	@Column(length = 18)
+	private String inscEstadual;
+	
+	@Column(length = 18)
+	private String inscMunicipal;	
+
+	@Column(length = 14)
+	private String cpf;
+
+	@Column(length = 12)
+	private String rg;		
+
+	@Column(length = 50, nullable = false)
+	private String endereco;
+	
+	@Column(length = 10, nullable = false)
+	private String cep;
+	
+	@Column(length = 50)
+	private String complemento;
+	
+	@Column(length = 13)
+	private String telefone;
+
+	@Column(length = 14)
+	private String celular1;
+
+	@Column(length = 14)
+	private String celular2;
+	
+	@Column(length = 50)
+	private String email;
+
+	@Column(length = 50)
+	private String webSite;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getContato() {
+		return contato;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getInscEstadual() {
+		return inscEstadual;
+	}
+
+	public void setInscEstadual(String inscEstadual) {
+		this.inscEstadual = inscEstadual;
+	}
+
+	public String getInscMunicipal() {
+		return inscMunicipal;
+	}
+
+	public void setInscMunicipal(String inscMunicipal) {
+		this.inscMunicipal = inscMunicipal;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular1() {
+		return celular1;
+	}
+
+	public void setCelular1(String celular1) {
+		this.celular1 = celular1;
+	}
+
+	public String getCelular2() {
+		return celular2;
+	}
+
+	public void setCelular2(String celular2) {
+		this.celular2 = celular2;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getWebSite() {
+		return webSite;
+	}
+
+	public void setWebSite(String webSite) {
+		this.webSite = webSite;
+	}
+	
+	
+}
---
+++ src/main/java/br/com/modulocge/main/HibernateTest.java
@@ -0,0 +1,24 @@
+package br.com.modulocge.main;
+
+import org.hibernate.Session;
+import br.com.modulocge.util.HibernateUtil;
+
+public class HibernateTest {
+	public static void main(String[] args) {
+		try {
+			//Capturar a sessão aberta pela fabrica de sessões da Class HibernateUtil e armazena no objeto sessao
+			Session sessao = HibernateUtil.getSessionFactory().openSession();
+			System.out.println("Conexão realizada com sucesso!");	
+		
+			//fecha a conexão
+			sessao.close();
+			//Destroi a fabrica de sessão
+			HibernateUtil.getSessionFactory().close();
+		} catch (Exception e) {
+			System.out.println("Falha na conexão, erro: "+ e);
+
+			//Destroi a fabrica de sessão
+			HibernateUtil.getSessionFactory().close();
+		}
+	}
+}
